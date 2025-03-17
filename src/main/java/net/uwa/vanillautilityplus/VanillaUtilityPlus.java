9ef384781c5a93f82ecb945b19cf6515857181b7
Mqc <87596784+MqcCheeze@users.noreply.github.com>
2025-03-17 18:50:05 +0000

    Added a command that allows for converting coordinates to the nether/overworld

--- src/main/java/net/uwa/vanillautilityplus/VanillaUtilityPlus.java
+++ src/main/java/net/uwa/vanillautilityplus/VanillaUtilityPlus.java
@@ -1,7 +1,11 @@
 package net.uwa.vanillautilityplus;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +16,19 @@ public class VanillaUtilityPlus implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal("pos")
+					.then(CommandManager.literal("nether")
+					.then(CommandManager.argument("x", IntegerArgumentType.integer())
+					.then(CommandManager.argument("y", IntegerArgumentType.integer())
+					.then(CommandManager.argument("z", IntegerArgumentType.integer())
+							.executes(context -> VanillaUtilityPlusCommands.convertCoordinates(context, "nether"))))))
+					.then(CommandManager.literal("overworld")
+					.then(CommandManager.argument("x", IntegerArgumentType.integer())
+					.then(CommandManager.argument("y", IntegerArgumentType.integer())
+					.then(CommandManager.argument("z", IntegerArgumentType.integer())
+							.executes(context -> VanillaUtilityPlusCommands.convertCoordinates(context, "overworld"))))))
+			);
+		});
 	}
 }
